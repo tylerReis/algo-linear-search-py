0ded5d2d7fcf563034a1da8798c690a3ed72cbe9
tylerReis <tyler.reis.j@outlook.com>
2024-02-01 17:02:35 -0500

    initial solution

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,20 @@
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    answer = 0
+    for x in array_to_search_through:
+        if x == value_to_find:
+            return answer
+        answer = answer + 1
 
 def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    answer_list = []
+    answer = 0
+    for x in array_to_search_through:
+        if value_to_find == x:
+            answer_list.append(answer) 
+        answer = answer + 1
+    return answer_list
+   
+
+
+print(linear_search(3, [1,2,3]))
+print(linear_search_global("a", ["b", "a", "n", "a", "n", "a", "s"]))
